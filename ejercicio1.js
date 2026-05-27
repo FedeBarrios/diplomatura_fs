14848252def55c315bbb71f48e88b19650f4c582
FedeBarrios <fede.barrios1987@gmail.com>
2026-05-27 05:40:09 -0300

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+const producto = {
+    artículo: "Laptop",
+    precio: 900000,
+    disponible: true,
+}
+console.log(`El artículo ${producto.artículo} tiene un precio de $${producto.precio} y está disponible: ${producto.disponible}`);
